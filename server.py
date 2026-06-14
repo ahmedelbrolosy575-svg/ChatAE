a8080eadb3027fb97afa8a0eab4f0eaaa2c36c4d
ahmedelbrolosy575-svg <ahmedelbrolosy575@gmail.com>
2026-06-14 18:59:06 +0300

    Update server.py

--- server.py
+++ server.py
@@ -24,3 +24,6 @@ async def websocket_endpoint(websocket: WebSocket, username: str):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
